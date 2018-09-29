bbd2d42d35a699c7a69e84faa972597c59714cf9
Lucas Frizon <lucasfrizongoulart@hotmail.com.br>
2018-09-28 23:39:35 -0300

    Tela inicial do programa

---
+++ tela_inicial.php
@@ -0,0 +1,124 @@
+<!-- inject:partials/header.html -->
+<!DOCTYPE html>
+<?php include_once('conexao.php');
+    
+    
+    ?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+     <link rel="shortcut icon" href="http://icons.iconarchive.com/icons/aha-soft/standard-new-year/256/Santa-cap-icon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <title>Página inicial</title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+
+    <!-- build:css -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- endbuild -->
+    
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i|Open+Sans:300,300i,400,400i,700,700i" rel="stylesheet">
+      
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+      
+    <link rel="stylesheet" href="assets/css/theme.css">
+      
+  </head>
+
+  <body>      
+  
+  
+      
+    <nav class="navbar navbar-expand-md navbar-custom fixed-top">
+        <div class="container">
+        
+            <button class="navbar-light navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsDefault" aria-controls="navbarsDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarsDefault">
+                <ul class="navbar-custom navbar-nav ml-auto">
+                    
+                    
+                  <left></left> <li class="nav-item">
+                    <a class="nav-link" href="sobre.php">Sobre nós</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                    <a class="nav-link" href="login.php">Administrador</a>
+                    </li>
+   
+                    <li class="nav-item">
+                        <a class="nav-link" href="http://example.com">Alimentos</a>
+                    </li> 
+                    
+                    <li class="nav-item active">
+                    <a class="nav-link" href="login.php" >Sair  <span class="sr-only">(current)</span></a>
+                    </li>
+                    
+                    
+                   
+                </ul>              
+                
+            </div>
+        </div>
+    </nav>
+<!-- endinject -->
+
+<!-- Intro -->
+<div class="intro">
+<div class="intro-body">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <h1 class="brand-heading">PEACH.</h1>
+        <p class="intro-text">
+        </p>
+        <a href="#pageid" class="btn btn-circle page-scroll">
+        <i class="fa fa-angle-double-down animated"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<!-- Section -->
+<section id="pageid">
+<div class="container content-section">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+            <h2 class="text-center">PEACH.</h2>
+      <p>A pobreza é a situação social e econômica na qual um indivíduo ou um grupo carece de certa forma dos elementos básicos para a subsistência pessoal. A pobreza é uma realidade que pode ser analisada a partir de várias perspectivas. Costuma-se dizer que uma nação é pobre quando a maioria da população não tem suas necessidades básicas atendidas. As necessidades básicas são a água, alimento e o agasalho, a habitação e os cuidados médicos.</p>
+            
+            <p>Tendo o uso de roupas e/ou agasalhos como necessidade básica do ser humano, torna-se fundamental a distribuição dos mesmos, principalmente em regiões de frio frequente ou em regiões em que desastres naturais são um problema recorrente. </p>
+
+            <p> No entanto, o controle de estoque e distribuição desses agasalhos é precário e quase inexistente , pois nem sempre as doações contemplam as famílias mais necessitadas.</p>
+
+    </div>
+  </div>
+</div>
+</section>
+
+
+<footer>
+<div class="container text-center">
+    <p class="credits">
+        Gabriel Gubert 2018<br/>
+        
+    </p>
+</div>
+</footer>
+
+<!-- build:js -->
+<script src="assets/js/main.js"></script>
+<!-- endbuild -->
+
+<script src="assets/js/theme.js"></script>
+
+</body>
+
+</html>
