fa4f9344772fa78eae7173783ae6b6940c9e4003
Lucas Frizon <lucasfrizongoulart@hotmail.com.br>
2018-09-29 13:46:11 -0300

    tela agrotoxicos

---
+++ agrotoxico.php
@@ -0,0 +1,171 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!-- details card section starts from here -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Agrotóxico</title>
+</head>
+<body>
+ <style>
+     /* card details start  */
+@import url('https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Roboto+Condensed:400,400i,700,700i');
+section{
+    padding: 100px 0;
+}
+.details-card {
+    background: #ecf0f1;
+}
+
+.card-content {
+    background: #ffffff;
+    border: 4px;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12);
+}
+
+.card-img {
+    position: relative;
+    overflow: hidden;
+    border-radius: 0;
+    z-index: 1;
+}
+
+.card-img img {
+    width: 100%;
+    height: auto;
+    display: block;
+}
+
+.card-img span {
+    position: absolute;
+    top: 15%;
+    left: 12%;
+    background: #1ABC9C;
+    padding: 6px;
+    color: #fff;
+    font-size: 12px;
+    border-radius: 4px;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    -ms-border-radius: 4px;
+    -o-border-radius: 4px;
+    transform: translate(-50%,-50%);
+}
+.card-img span h4{
+        font-size: 12px;
+        margin:0;
+        padding:10px 5px;
+         line-height: 0;
+}
+.card-desc {
+    padding: 1.25rem;
+}
+
+.card-desc h3 {
+    color: #000000;
+    font-weight: 600;
+    font-size: 1.5em;
+    line-height: 1.3em;
+    margin-top: 0;
+    margin-bottom: 5px;
+    padding: 0;
+}
+
+.card-desc p {
+    color: #747373;
+    font-size: 14px;
+    font-weight: 400;
+    font-size: 1em;
+    line-height: 1.5;
+    margin: 0px;
+    margin-bottom: 20px;
+    padding: 0;
+    font-family: 'Raleway', sans-serif;
+}
+.btn-card{
+    background-color: #1ABC9C;
+    color: #fff;
+    box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 10px 0 rgba(0,0,0,.12);
+    padding: .84rem 2.14rem;
+    font-size: .81rem;
+    -webkit-transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+    -o-transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out;
+    margin: 0;
+    border: 0;
+    -webkit-border-radius: .125rem;
+    border-radius: .125rem;
+    cursor: pointer;
+    text-transform: uppercase;
+    white-space: normal;
+    word-wrap: break-word;
+    color: #fff;
+}
+.btn-card:hover {
+    background: orange;
+}
+a.btn-card {
+    text-decoration: none;
+    color: #fff;
+}
+/* End card section */
+ </style>
+</body>
+</html>
+<center><h1><b>BRASIL, CAMPEÃO MUNDIAL EM CONSUMO DE AGROTÓXICOS</b></h1></center>
+<section class="details-card">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="agrotoxico.jpg" height="50" alt="">
+                    </div>
+                    <div class="card-desc">
+                        <h3>O uso do agrotóxico no Brasil</h3>
+                        <p><i>Segundo dossiê publicado pela ABRASCO – Associação Brasileira de Saúde Coletiva e realizado em conjunto com o Ministério da Saúde: 64% dos alimentos no Brasil são contaminados por agrotóxicos; 34.147 intoxicações por esses produtos foram notificadas no SUS entre 2007 e 2014; 288% foi o percentual de aumento do uso dos agrotóxicos no Brasil entre 2000 e 2012 e o faturamento da indústria de agrotóxicos no Brasil em 2014 foi de 12 bilhões de dólares.</i></p>
+                               
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="agromata.jpg" alt="">
+                        
+                    </div>
+                    <div class="card-desc">
+                        <ul>
+                        <h3>Problemas causados à saúde pelo agrotóxico</h3>
+                        <li><p>Possível causa de câncer</p></li>
+                        <li><p>Sintomas de intoxicação</p></li>
+                        <li><p>Afetam nosso cérebro</p></li>
+                        <li><p><i>Também afeta a taxa de fertilidade!!!</i></p></li>
+                        </ul>      
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card-content">
+                    <div class="card-img">
+                        <img src="imagemagro.jpg" alt="">
+                        
+                    </div>
+                    <div class="card-desc">
+                        <h3>Fiscalização</h3>
+                        <i><p>No ano de 2002, a Agência Nacional de Vigilância Sanitária (ANVISA) criou o programa de Análise de Resíduos de Agrotóxicos em Alimentos (PARA), visando monitorar o comprimento da legislação sobre o nível permitido de resíduos agrotóxicos nos alimentos, o uso de quais produtos é permitido em cada colheita, e garantir que produtos como frutas, verduras e legumes cheguem à mesa do consumidor brasileiro com qualidade e segurança.
+
+</p></i>
+                               
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- details card section starts from here -->
