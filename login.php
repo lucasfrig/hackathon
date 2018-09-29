c05cda9e7661926f333281a3b3afb8a398d64ba6
Lucas Frizon <lucasfrizongoulart@hotmail.com.br>
2018-09-28 21:14:13 -0300

    versão inicial tela de login adm

---
+++ login.php
@@ -0,0 +1,101 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="http://icons.iconarchive.com/icons/artbees/paradise-fruits/256/Peach-icon.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <title>Tela de login</title>
+    
+    <style type="text/css">
+
+@import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+.login-block{
+    background: #2E8B57;  
+background: -webkit-linear-gradient(to bottom, #00EE76, #2E8B57);  
+background: linear-gradient(to bottom, #00EE76, #2E8B57); 
+float:left;
+width:100%;
+padding : 50px 0;
+}
+.banner-sec{background:url(https://static.pexels.com/photos/33972/pexels-photo.jpg)  no-repeat left bottom; background-size:cover; min-height:500px; border-radius: 0 10px 10px 0; padding:0;}
+.container{background:#fff; border-radius: 10px; box-shadow:15px 20px 0px rgba(0,0,0,0.1);}
+.carousel-inner{border-radius:0 10px 10px 0;}
+.carousel-caption{text-align:left; left:5%;}
+.login-sec{padding: 50px 30px; position:relative;}
+.login-sec .copy-text{position:absolute; width:80%; bottom:20px; font-size:13px; text-align:center;}
+.login-sec .copy-text i{color:##698B22;}
+.login-sec .copy-text a{color:#698B22;}
+.login-sec h2{margin-bottom:30px; font-weight:800; font-size:30px; color: #698B22;}
+.login-sec h2:after{content:" "; width:100px; height:5px; background:#008B45; display:block; margin-top:20px; border-radius:3px; margin-left:auto;margin-right:auto}
+.btn-login{background: #698B22; color:#fff; font-weight:600;}
+.banner-text{width:70%; position:absolute; bottom:40px; padding-left:20px;}
+.banner-text h2{color:#fff; font-weight:600;}
+.banner-text h2:after{content:" "; width:100px; height:5px; background:#FFF; display:block; margin-top:20px; border-radius:3px;}
+.banner-text p{color:#fff;}
+
+</style>
+    
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<section class="login-block">
+    <div class="container">
+	<div class="row">
+		<div class="col-md-4 login-sec">
+		    <h2 class="text-center">Login</h2>
+		    <form class="login-form">
+  <div class="form-group">
+    <label for="exampleInputEmail1" >Nome do administrador</label>
+    <input type="text" class="form-control" placeholder="">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" >Senha</label>
+    <input type="password" class="form-control" placeholder="">
+  </div>
+  
+  
+ 
+</form>
+<div class="copy-text">Criado por Lucas Frizon, Artur Quatrin e Gabriel Gubert</div>
+		</div>
+		<div class="col-md-8 banner-sec">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                   
+                    
+                  </ol>
+            <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active">
+      <img class="d-block img-fluid" src="fruta.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+        <center><div class="banner-text"> </center>
+            <h2>Peach</h2>
+            <p>Enriquecendo sua vida de forma saudável</p>
+        </div>	
+  </div>
+    </div>
+
+            </div>	   
+		    
+		</div>
+	</div>
+</div>
+</section>
+   
+  </head>
+  <body>
+  
+  </body>
+</html>
+
+
+</html>
+
